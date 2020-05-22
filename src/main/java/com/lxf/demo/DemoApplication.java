98edcde73e9d455d5dfa79f38559d93f0ced3e8a
2355927102@shboka <2355927102@qq.com>
2020-05-22 18:14:23 +0800

    添加注释

--- src/main/java/com/lxf/demo/DemoApplication.java
+++ src/main/java/com/lxf/demo/DemoApplication.java
@@ -11,6 +11,7 @@ public class DemoApplication {
     public static void main(String[] args) {
         System.out.println("启动spring boot");
         SpringApplication.run(DemoApplication.class, args);
+        System.out.println("启动DemoApplication完成....");
     }
 
 }
