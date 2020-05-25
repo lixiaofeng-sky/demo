c65a6df2736d35e8a6b0a1cacf15b9780e5e7306
2355927102@shboka <2355927102@qq.com>
2020-05-25 17:18:51 +0800

    添加user类

---
+++ src/main/java/com/lxf/demo/modal/User.java
@@ -0,0 +1,17 @@
+package com.lxf.demo.modal;
+
+/**
+ * @author Lixf
+ * @date 2020/5/25 15:26
+ */
+public class User {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
